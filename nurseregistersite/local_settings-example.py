df3d320538e69b4d2ee16218c5d0747c269ec4a4
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2018-07-09 13:53:38 +0200

    Add local_settings example file

---
+++ nurseregistersite/local_settings-example.py
@@ -0,0 +1,11 @@
+!# Use this file to override settings from the settings.py file
+
+# Set up a local secret_key to protect user passwords and sessions
+# Can be generated with:
+#  python -c "import random; print(
+#    ''.join([
+#       random.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
+#       for i in range(50)
+#   ])
+#  )"
+SECRET_KEY = '_*$syc#$uml4taqpra2+5hm4($yovr4of(vwlu=#k4z4(g^)6l'
